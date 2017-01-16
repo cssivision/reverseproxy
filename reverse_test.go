74e9518ba1ec31545818423d6a0f2319d5b365b8
cssivision <cssivision@gmail.com>
2017-01-17 00:35:06 +0800

    fix: format code

--- reverse_test.go
+++ reverse_test.go
@@ -1,127 +1,124 @@
 package reverseproxy
 
 import (
-    "testing"
-    "net/http"
-    "net/url"
-    "io/ioutil"
-    "net/http/httptest"
-    "reflect"
-    "log"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"reflect"
+	"testing"
 )
 
 const fakeHopHeader = "X-Fake-Hop-Header-For-Test"
 
 func init() {
-    hopHeaders = append(hopHeaders, fakeHopHeader)
+	hopHeaders = append(hopHeaders, fakeHopHeader)
 }
 
 func TestReverseProxy(t *testing.T) {
-    backendResponse := "I am the backend"
-    backendStatus := 404
-    backend := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-        if len(req.TransferEncoding) > 0 {
-            t.Errorf("backend got unexpected TransferEncoding: %v", req.TransferEncoding)
-        }
+	backendResponse := "I am the backend"
+	backendStatus := 404
+	backend := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		if len(req.TransferEncoding) > 0 {
+			t.Errorf("backend got unexpected TransferEncoding: %v", req.TransferEncoding)
+		}
 
-        if req.Header.Get("X-Forwarded-For") == "" {
-            t.Errorf("didn't get X-Forwarded-For header")
-        }
+		if req.Header.Get("X-Forwarded-For") == "" {
+			t.Errorf("didn't get X-Forwarded-For header")
+		}
 
-        if c := req.Header.Get("Connection"); c != "" {
-            t.Errorf("handler got Connection header value %q", c)
-        }
+		if c := req.Header.Get("Connection"); c != "" {
+			t.Errorf("handler got Connection header value %q", c)
+		}
 
-        if c := req.Header.Get("Upgrade"); c != "" {
-            t.Errorf("handler got Upgrade header value %q", c)
-        }
+		if c := req.Header.Get("Upgrade"); c != "" {
+			t.Errorf("handler got Upgrade header value %q", c)
+		}
 
-        if c := req.Header.Get("Proxy-Connection"); c != "" {
-            t.Errorf("handler got Proxy-Connection header value %q", c)
-        }
+		if c := req.Header.Get("Proxy-Connection"); c != "" {
+			t.Errorf("handler got Proxy-Connection header value %q", c)
+		}
 
-        if g, e := req.Host, ""; g == e {
-            t.Errorf("backend got Host header %q, want %q", g, e)
-        }
+		if g, e := req.Host, ""; g == e {
+			t.Errorf("backend got Host header %q, want %q", g, e)
+		}
 
-        rw.Header().Set("X-Foo", "bar")
-        rw.Header().Set(fakeHopHeader, "foo")
-        rw.Header().Set("Trailers", "not a special header field name")
-        rw.Header().Set("Trailer", "X-Trailer")
-        rw.Header().Set("Upgrade", "foo")
-        rw.Header().Add("X-Multi-Value", "foo")
-        rw.Header().Add("X-Multi-Value", "bar")
-        http.SetCookie(rw, &http.Cookie{Name: "flavor", Value: "chocolateChip"})
-        rw.WriteHeader(backendStatus)
-        rw.Write([]byte(backendResponse))
-        rw.Header().Set("X-Trailer", "trailer_value")
-    }))
+		rw.Header().Set("X-Foo", "bar")
+		rw.Header().Set(fakeHopHeader, "foo")
+		rw.Header().Set("Trailers", "not a special header field name")
+		rw.Header().Set("Trailer", "X-Trailer")
+		rw.Header().Set("Upgrade", "foo")
+		rw.Header().Add("X-Multi-Value", "foo")
+		rw.Header().Add("X-Multi-Value", "bar")
+		http.SetCookie(rw, &http.Cookie{Name: "flavor", Value: "chocolateChip"})
+		rw.WriteHeader(backendStatus)
+		rw.Write([]byte(backendResponse))
+		rw.Header().Set("X-Trailer", "trailer_value")
+	}))
 
-    defer backend.Close()
-    backendURL, err := url.Parse(backend.URL)
-    if err != nil {
-        t.Fatal(err)
-    }
+	defer backend.Close()
+	backendURL, err := url.Parse(backend.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-    proxyHandler := NewReverseProxy(backendURL)
-    proxyHandler.ErrorLog = log.New(ioutil.Discard, "", 0)
-    frontend := httptest.NewServer(proxyHandler)
-    defer frontend.Close()
+	proxyHandler := NewReverseProxy(backendURL)
+	proxyHandler.ErrorLog = log.New(ioutil.Discard, "", 0)
+	frontend := httptest.NewServer(proxyHandler)
+	defer frontend.Close()
 
-    getReq, _ := http.NewRequest("GET", frontend.URL, nil)
-    getReq.Host = "some host"
-    getReq.Header.Set("Connection", "close")
-    getReq.Header.Set("Proxy-Connection", "should be deleted")
-    getReq.Header.Set("Upgrade", "foo")
-    getReq.Close = true
-    res, err := http.DefaultClient.Do(getReq)
-    if err != nil {
-        t.Fatalf("Get: %v", err)
-    }
+	getReq, _ := http.NewRequest("GET", frontend.URL, nil)
+	getReq.Host = "some host"
+	getReq.Header.Set("Connection", "close")
+	getReq.Header.Set("Proxy-Connection", "should be deleted")
+	getReq.Header.Set("Upgrade", "foo")
+	getReq.Close = true
+	res, err := http.DefaultClient.Do(getReq)
+	if err != nil {
+		t.Fatalf("Get: %v", err)
+	}
 
-    if g, e := res.StatusCode, backendStatus; g != e {
-        t.Errorf("got res.StatusCode %d; expected %d", g, e)
-    }
+	if g, e := res.StatusCode, backendStatus; g != e {
+		t.Errorf("got res.StatusCode %d; expected %d", g, e)
+	}
 
-    if g, e := res.Header.Get("X-Foo"), "bar"; g != e {
-        t.Errorf("got X-Foo %q; expected %q", g, e)
-    }
+	if g, e := res.Header.Get("X-Foo"), "bar"; g != e {
+		t.Errorf("got X-Foo %q; expected %q", g, e)
+	}
 
-    if c := res.Header.Get(fakeHopHeader); c != "" {
-        t.Errorf("got %s header value %q", fakeHopHeader, c)
-    }
+	if c := res.Header.Get(fakeHopHeader); c != "" {
+		t.Errorf("got %s header value %q", fakeHopHeader, c)
+	}
 
-    if g, e := res.Header.Get("Trailers"), "not a special header field name"; g != e {
-        t.Errorf("header Trailers = %q; want %q", g, e)
-    }
+	if g, e := res.Header.Get("Trailers"), "not a special header field name"; g != e {
+		t.Errorf("header Trailers = %q; want %q", g, e)
+	}
 
-    if g, e := len(res.Header["X-Multi-Value"]), 2; g != e {
-        t.Errorf("got %d X-Multi-Value header values; expected %d", g, e)
-    }
+	if g, e := len(res.Header["X-Multi-Value"]), 2; g != e {
+		t.Errorf("got %d X-Multi-Value header values; expected %d", g, e)
+	}
 
-    if g, e := len(res.Header["Set-Cookie"]), 1; g != e {
-        t.Fatalf("got %d SetCookies, want %d", g, e)
-    }
+	if g, e := len(res.Header["Set-Cookie"]), 1; g != e {
+		t.Fatalf("got %d SetCookies, want %d", g, e)
+	}
 
-    if g, e := res.Trailer, (http.Header{"X-Trailer": nil}); !reflect.DeepEqual(g, e) {
-        t.Errorf("before reading body, Trailer = %#v; want %#v", g, e)
-    }
+	if g, e := res.Trailer, (http.Header{"X-Trailer": nil}); !reflect.DeepEqual(g, e) {
+		t.Errorf("before reading body, Trailer = %#v; want %#v", g, e)
+	}
 
-    if cookie := res.Cookies()[0]; cookie.Name != "flavor" {
-        t.Errorf("unexpected cookie %q", cookie.Name)
-    }
+	if cookie := res.Cookies()[0]; cookie.Name != "flavor" {
+		t.Errorf("unexpected cookie %q", cookie.Name)
+	}
 
-    bodyBytes, _ := ioutil.ReadAll(res.Body)
+	bodyBytes, _ := ioutil.ReadAll(res.Body)
 
-    if g, e := string(bodyBytes), backendResponse; g != e {
-        t.Errorf("got body %q; expected %q", g, e)
-    }
+	if g, e := string(bodyBytes), backendResponse; g != e {
+		t.Errorf("got body %q; expected %q", g, e)
+	}
 
-    if g, e := res.Trailer.Get("X-Trailer"), "trailer_value"; g != e {
-        t.Errorf("Trailer(X-Trailer) = %q ; want %q", g, e)
-    }
+	if g, e := res.Trailer.Get("X-Trailer"), "trailer_value"; g != e {
+		t.Errorf("Trailer(X-Trailer) = %q ; want %q", g, e)
+	}
 
 }
-
-
-
